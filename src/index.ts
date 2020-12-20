1b7038aed89ab0bfd69a02048c0e12d124b77f07
angela <angela.liu@axcelerate.com.au>
2020-12-20 19:16:00 +1000

    finish views&models

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const collection = User.buildUserCollection();
+const users = new Collection('http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  });
 
-collection.on('change', () => {
-  console.log(collection);
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
 });
 
-collection.fetch();
+users.fetch();
+
